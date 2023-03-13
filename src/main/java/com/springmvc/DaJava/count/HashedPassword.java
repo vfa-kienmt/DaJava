8ae46e0a3ada21852b98c05a1af2c0365ed47841
vfa-kienmt <109578772+vfa-kienmt@users.noreply.github.com>
2023-03-13 15:16:40 +0700

    feature staff admin

--- src/main/java/com/springmvc/DaJava/count/HashedPassword.java
+++ src/main/java/com/springmvc/DaJava/count/HashedPassword.java
@@ -2,6 +2,8 @@ package com.springmvc.DaJava.count;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Random;
+
 public class HashedPassword {
     public static String hash(String password) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
@@ -17,5 +19,28 @@ public class HashedPassword {
         } else {
             return false;
         }
+    }
+
+    public static String RandomPassord() {
+        String password = hash(Random());
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(password);
+        return hashedPassword;
+    }
+
+    public static String Random() {
+        String password = generateRandomPassword(8);
+        return password;
+    }
+
+    private static String generateRandomPassword(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random rand = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int index = rand.nextInt(characters.length());
+            sb.append(characters.charAt(index));
+        }
+        return sb.toString();
     }
 }
